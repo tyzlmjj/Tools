34817c8453dbed279083e11010abfd16e9dea330
MJJ <tyzl931019@gmail.com>
2016-08-18 09:40:23 +0800

    update BitmapUtils

--- Android/BitmapUtils.java
+++ Android/BitmapUtils.java
@@ -1,6 +1,7 @@
 
 import android.annotation.TargetApi;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Build;
 
 /**
@@ -13,13 +14,13 @@ public class BitmapUtils
      */
     @TargetApi(Build.VERSION_CODES.KITKAT)
     public static int getBitmapByteSize(Bitmap bitmap) {
-        // The return value of getAllocationByteCount silently changes for recycled bitmaps from the
-        // internal buffer size to row bytes * height. To avoid random inconsistencies in caches, we
-        // instead assert here.
+
+        //检查bitmap是否被销毁
         if (bitmap.isRecycled()) {
             throw new IllegalStateException("Cannot obtain size for recycled Bitmap: " + bitmap
                     + "[" + bitmap.getWidth() + "x" + bitmap.getHeight() + "] " + bitmap.getConfig());
         }
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             // Workaround for KitKat initial release NPE in Bitmap, fixed in MR1. See issue #148.
             try {
@@ -34,9 +35,35 @@ public class BitmapUtils
     /**
      * 获取{@link Bitmap}在内存中占用的字节大小
      */
-    public static int getBitmapByteSize(int width, int height, Bitmap.Config config) {
+    public static int getBitmapByteSize(int width, int height, Bitmap.Config config)
+    {
         return width * height * getBytesPerPixel(config);
     }
+
+    /**
+     * 检查图片的宽高
+     */
+    public static int[] getImageDimensions(String path)
+    {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(path,options);
+        int imageHeight = options.outHeight;
+        int imageWidth = options.outWidth;
+        return new int[]{imageWidth,imageHeight};
+    }
+
+    /**
+     * 检查图片类型
+     */
+    public static String getImageType(String path)
+    {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(path,options);
+        return options.outMimeType;
+    }
+
 
     private static int getBytesPerPixel(Bitmap.Config config) {
         // A bitmap by decoding a gif has null "config" in certain environments.
